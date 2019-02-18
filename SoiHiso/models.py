31eb9055f3e2a8fd0ac2cd3b5e04a2a41909c4d0
unknown <Mewkkn@hotmail.com>
2019-02-18 16:21:57 +0700

    Model Menu and Review

--- SoiHiso/models.py
+++ SoiHiso/models.py
@@ -9,9 +9,21 @@ class TypeFood(models.Model):
     def __str__(self):
         return self.typefood_text
 
-
 class Name(models.Model):
     typefood = models.ForeignKey(TypeFood, on_delete=models.CASCADE)
     name_text = models.CharField(max_length=200)
     def __str__(self):
         return self.name_text
+
+class Menu(models.Model):
+    menu = models.ForeignKey(Name, on_delete= models.CASCADE)
+    menu_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.menu_text
+
+class Review(models.Model):
+    restaurant = models.ForeignKey(Name, on_delete=models.CASCADE)
+    point = models.IntegerField(default=0)
+    review_date = models.DateTimeField('date published')
+    review_text = models.CharField(max_length=300)
+    summary_text = models.CharField(max_length=500)
