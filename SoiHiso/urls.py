c9e239b06046f4db8c701d10b8cf30443ec66ca0
unknown <Mewkkn@hotmail.com>
2019-03-02 21:36:06 +0700

    Change from Generic views to def

--- SoiHiso/urls.py
+++ SoiHiso/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'SoiHiso'
 urlpatterns = [
     path('',views.Homepage, name='homepage'),
-    path('SoiHiso/<int:pk>/',  views.DetailView.as_view(), name='detail'),
-    path('typefood/', views.Type.as_view(), name='typefood'),
-    path('restaurant/', views.restaurant, name='restaurant'),
+    path('SoiHiso/<int:name_id>/', views.detail_restaurant, name='detail'),
+    path('typefood/', views.type, name='typefood'),
     path('<int:name_id>/menu/', views.menu, name='menu'),
+    path('restaurant/', views.restaurant, name='restaurant'),
     path('search/', views.search, name='search'),
+    path('popular/', views.popular, name='popular'),
     path('<int:name_id>/NameRes/', views.NameRes, name='NameRes'),
     path('reviews/', views.AddReview, name='review')
 
--- SoiHiso/views.py
+++ SoiHiso/views.py
@@ -1,29 +1,25 @@
 from django.shortcuts import render, redirect
 from django.views import generic
-from .models import TypeFood, Name,Review
+from .models import TypeFood, Restaurant,Review,Menu
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
-from django.http import HttpResponseRedirect ,HttpResponse
-from django.template import loader
+
 
 # Create your views here.
 def Homepage(request):
     return render(request, 'homepage.html')
 
-class Type(generic.ListView):
-    template_name = 'type.html'
-    context_object_name = 'latest_typefood_list'
+def type(request):
+    typefood = TypeFood.objects.all()
+    return render(request, 'type.html', {'typefood':typefood})
 
-    def get_queryset(self):
-        return TypeFood.objects.all()
-
-class DetailView(generic.DetailView):
-    model = TypeFood
-    template_name = 'detail.html'
+def detail_restaurant(request,name_id):
+    typefood = get_object_or_404(TypeFood, pk=name_id)
+    return render(request, 'detail.html', {'typefood': typefood})
 
 def menu(request,name_id):
-    name = get_object_or_404(Name, pk=name_id)
+    name = get_object_or_404(Restaurant, pk=name_id)
     return render(request, 'menu.html', {'name': name})
 
 def restaurant(request):
@@ -31,13 +27,13 @@ def restaurant(request):
 
 def search (request):
     keyword = (request.POST['name'])
-    namekeword = Name.objects.values_list("id", "name_text").filter(name_text__startswith = keyword)
+    namekeword = Restaurant.objects.values_list("id", "name_text").filter(name_text__startswith = keyword)
     count = len(namekeword)
     context = {'lstname':namekeword,'count':count,'key':keyword}
     return  render(request,'search.html',context)
 
 def NameRes(request,name_id):
-    name = get_object_or_404(Name, pk=name_id)
+    name = get_object_or_404(Restaurant, pk=name_id)
     reviews = Review.objects.values_list("restaurant_id", "review_text", "point").filter(restaurant_id = name.id)
     context = {'name': name, 'reviews' : reviews}
     return render(request, 'reviews.html', context)
@@ -48,4 +44,14 @@ def AddReview(request):
     review_detail = str(request.POST['review_detail'])
     name = Review(restaurant_id=name_restuarant,point=points,review_date=timezone.now(),review_text=review_detail)
     name.save()
-    return redirect('SoiHiso:NameRes', 1)
+    return redirect('SoiHiso:NameRes',name_restuarant )
+
+def popular(request):
+    names = Restaurant.objects.values_list("id", "name_text")
+    length_names = len(names) + 1
+    avg_star = [0] * length_names
+    get_avg = [0] * length_names
+    for id in range(1,length_names):
+        avg_star[id] =  Review.objects.values_list("restaurant_id", "point").filter(restaurant_id = id)
+    context = {'names': names}
+    return render(request, 'popular.html', context)
